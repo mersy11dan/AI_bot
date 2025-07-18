d80297b78c97d8292104568e328b17e7f59b2378
mersy11dan <mersy11dan@gmail.com>
2025-07-18 16:10:54 +0500

    updated dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,24 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  // Pre-bundle react‑icons so Rollup can see every sub‑path
   optimizeDeps: {
     include: [
       'react-icons',
       'react-icons/fa',
       'react-icons/fi'
     ]
-  },
-
-  // (Only if you truly want to externalize them in your build)
-  build: {
-    rollupOptions: {
-      external: [
-        // remove these lines if you want the icons bundled into your app
-        'react-icons',
-        'react-icons/fa',
-        'react-icons/fi'
-      ]
-    }
   }
 })
